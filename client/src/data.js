74b9933bb950aff6a6aae3b6a0de4d5ff8ce801d
Laurynas Rup <lauryfas@gmail.com>
2021-02-05 13:32:45 +0000

    External Media

--- client/src/data.js
+++ client/src/data.js
@@ -1,19 +1,3 @@
-// Images
-import Brewdog from './img/BrewdogImg.png';
-import Restaurant from './img/Restaurant.png';
-import Trckr from './img/FootballTrckr.png';
-import Planted from './img/Planted.png';
-// Desktop Videos
-import PunkDesktop from './video/PunkDesktop.mp4';
-import RestaurantDesktop from './video/RestaurantDesktop.mp4';
-import TrckrDesktop from './video/TrckrDesktop.mp4';
-import PlantedDesktop from './video/PlantedDesktop.mp4';
-// Mobile Videos
-import PunkMobile from './video/PunkMobile.mp4';
-import RestaurantMobile from './video/RestaurantMobile.mp4';
-import TrckrMobile from './video/TrckrMobile.mp4';
-import PlantedMobile from './video/PlantedMobile.mp4';
-
 export const projectsData = [
 	{
 		id: 1,
@@ -23,9 +7,12 @@ export const projectsData = [
 		longDesc:
 			'This is a single page web application developed using HTML, CSS and Vanilla JS. It is build in accordance to the MVC design pattern and is bundled with the help of Webpack module bundler. The app returns a list of beers, a single or a random beer from an external API based on user input. The application also features pagination functionality as well as liking capability, implemented via local storage.',
 		media: {
-			img: Brewdog,
-			desktopVid: PunkDesktop,
-			mobileVid: PunkMobile,
+			img:
+				'https://res.cloudinary.com/laurynasrup/image/upload/f_auto,q_auto:best/v1612529879/Portfolio%20Images/BrewdogImg_m7q3sx.png',
+			desktopVid:
+				'https://res.cloudinary.com/laurynasrup/video/upload/f_auto,q_auto:low/v1612529958/Portfolio%20Videos/PunkDesktop_r9d0ow.mp4',
+			mobileVid:
+				'https://res.cloudinary.com/laurynasrup/video/upload/f_auto,q_auto:low/v1612529948/Portfolio%20Videos/PunkMobile_nxcewu.mp4',
 		},
 		links: {
 			github: 'https://github.com/LaurynasRup/beer_matcher',
@@ -40,9 +27,12 @@ export const projectsData = [
 		longDesc:
 			'This is a multiple page informative website, built with HTML, CSS and Vanilla JS. It is structured with one CSS file and multiple HTML pages each paired with a designated JS file. The website utilises multiple automatic image carousels coupled with image navs. It also involves a fictional restaurant booking functionality, featuring multiple forms and employing a client side validation.',
 		media: {
-			img: Restaurant,
-			desktopVid: RestaurantDesktop,
-			mobileVid: RestaurantMobile,
+			img:
+				'https://res.cloudinary.com/laurynasrup/image/upload/f_auto,q_auto:best/v1612529879/Portfolio%20Images/Restaurant_vtuojf.png',
+			desktopVid:
+				'https://res.cloudinary.com/laurynasrup/video/upload/f_auto,q_auto:low/v1612529963/Portfolio%20Videos/RestaurantDesktop_njzlfz.mp4',
+			mobileVid:
+				'https://res.cloudinary.com/laurynasrup/video/upload/f_auto,q_auto:low/v1612529956/Portfolio%20Videos/RestaurantMobile_tafhzw.mp4',
 		},
 		links: {
 			github: 'https://github.com/LaurynasRup/ruppo_restaurant',
@@ -57,9 +47,12 @@ export const projectsData = [
 		longDesc:
 			'This is a full stack multi-page web-application. Client side is built with JS, CSS & HTML and bundled with Webpack. The server side is built with Node JS, Express & MySql. The app includes encrypted sign up and login features and allows user to store footbal leagues to their account. It provides data of current league table, scores of previous matches and schedule of upcominig fixtures, all retrieved from an external API. ',
 		media: {
-			img: Trckr,
-			desktopVid: TrckrDesktop,
-			mobileVid: TrckrMobile,
+			img:
+				'https://res.cloudinary.com/laurynasrup/image/upload/f_auto,q_auto:best/v1612529880/Portfolio%20Images/FootballTrckr_hmuq5c.png',
+			desktopVid:
+				'https://res.cloudinary.com/laurynasrup/video/upload/f_auto,q_auto:low/v1612529958/Portfolio%20Videos/TrckrDesktop_r6zwhc.mp4',
+			mobileVid:
+				'https://res.cloudinary.com/laurynasrup/video/upload/f_auto,q_auto:low/v1612529951/Portfolio%20Videos/TrckrMobile_mmbbpw.mp4',
 		},
 		links: {
 			github: 'https://github.com/LaurynasRup/trckr',
@@ -74,9 +67,12 @@ export const projectsData = [
 		longDesc:
 			'This is a fictional plant e-shop website, built with React JS. It provides a multi-page experience implemented with React-Router, a product filtering functioanlity, as well as shopping basket features. The website incorporates scroll as well as page transition animations, implemented with Framer Motion library. It includes a few forms, which simulates checking-out process and are validated on the client side.',
 		media: {
-			img: Planted,
-			desktopVid: PlantedDesktop,
-			mobileVid: PlantedMobile,
+			img:
+				'https://res.cloudinary.com/laurynasrup/image/upload/f_auto,q_auto:best/v1612529876/Portfolio%20Images/Planted_gnurgl.png',
+			desktopVid:
+				'https://res.cloudinary.com/laurynasrup/video/upload/f_auto,q_auto:low/v1612529963/Portfolio%20Videos/PlantedDesktop_fdx1pl.mp4',
+			mobileVid:
+				'https://res.cloudinary.com/laurynasrup/video/upload/f_auto,q_auto:low/v1612529953/Portfolio%20Videos/PlantedMobile_h29y8f.mp4',
 		},
 		links: {
 			github: 'https://github.com/LaurynasRup/planted/tree/master/planted',
